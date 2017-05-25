0ec64a877f9967939093847ee888ff3eed5180ff
Chris Traganos <ctraganos@gmail.com>
2017-05-25 11:35:58 -0500

    First pass at the basic app

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.listen(3000, function(){
+  console.log('listening on 3000');
+})
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/index.html')
+  //Note: __dirname is directory that containts the JS soure code. Try logging it and see what you get
+  console.log(__dirname);
+})
