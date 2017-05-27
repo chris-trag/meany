bb88d46c2728bbbe3188b5006b67da19a71ebf0d
Chris Traganos <ctraganos@gmail.com>
2017-05-27 00:06:48 -0500

    Connecting to MongoDB and pulling back down the documents

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+// Connect to MongoDB
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
 
-app.listen(3000, function(){
-  console.log('listening on 3000');
+// All the Event handlers below
+
+var db;
+
+MongoClient.connect('mongodb://ctraganos:passport@ds015878.mlab.com:15878/mean-stack', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
 
-app.get('/', (req,res) => {
-  res.sendFile(__dirname + '/index.html')
-  //Note: __dirname is directory that containts the JS soure code. Try logging it and see what you get
-  console.log(__dirname);
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
